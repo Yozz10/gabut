553bece054ac6dab5a716577bc11a2de2fa6b48b
Yozz10 <siestachan1445@gmail.com>
2025-10-26 12:52:30 +0700

    Create App component with Linkboard integration

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Linkboard from "./Linkboard";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-pink-200 via-pink-300 to-pink-400 text-gray-800">
+      <Linkboard />
+    </div>
+  );
+}
