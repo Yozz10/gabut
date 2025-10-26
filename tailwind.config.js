ec1c19b62d0c63b210c21a680afb69766c99b9fe
Yozz10 <siestachan1445@gmail.com>
2025-10-26 13:10:41 +0700

    Update Tailwind config with custom colors
    Added custom pinky color shades to the theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pinky: {
+          light: "#fbcfe8",
+          DEFAULT: "#f472b6",
+          dark: "#ec4899",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
