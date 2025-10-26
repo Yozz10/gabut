604133b0372610e2c1624df52a9de03790697a0d
Yozz10 <siestachan1445@gmail.com>
2025-10-26 12:37:41 +0700

    Initialize main.jsx with React and Linkboard component

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Linkboard from './components/Linkboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Linkboard />
+  </React.StrictMode>,
+)
